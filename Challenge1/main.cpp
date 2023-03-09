db801799453a94e8650e57d9611dcaf96e99caa3
theskidster <theskidster@gmail.com>
2023-03-08 20:46:50 -0600

    Added Challenge1 to directory

---
+++ Challenge1/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: theskidster
+ *
+ * Created on March 8, 2023, 7:05 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello World!");
+    
+    return 0;
+}
+
